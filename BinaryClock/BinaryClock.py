268323b539395fa3d8c5868c38ae09ed8c488d5d
Lane Moseley <moseley.lane@gmail.com>
2018-07-24 10:05:24 -0600

    Clock background and time indicators added.

--- BinaryClock/BinaryClock.py
+++ BinaryClock/BinaryClock.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from datetime import datetime
 
-
 def binary_time( hours, minutes, seconds ):
     
     # Variables
@@ -34,7 +33,7 @@ def binary_time( hours, minutes, seconds ):
         elif time[0] % 2 == 0:
             binary_clock[2][1] = "[X]"
             time[0] -= 2
-        elif time[0] == 1:
+        else:
             binary_clock[3][1] = "[X]"
             time[0] -= 1
     
@@ -67,7 +66,7 @@ def binary_time( hours, minutes, seconds ):
         elif time[1] % 2 == 0:
             binary_clock[2][3] = "[X]"
             time[1] -= 2
-        elif time[1] % 1 == 0 and time[1] > 0:
+        else:
             binary_clock[3][3] = "[X]"
             time[1] -= 1
    
@@ -100,7 +99,7 @@ def binary_time( hours, minutes, seconds ):
         elif time[2] % 2 == 0:
             binary_clock[2][5] = "[X]"
             time[2] -= 2
-        elif time[2] % 1 == 0 and time[2] > 0:
+        else:
             binary_clock[3][5] = "[X]"
             time[2] -= 1
     
@@ -124,12 +123,71 @@ def main():
     second = now.second
     binary_time(hour, minute, second)
     
+
+    # Tkinter
     BinaryClock = Tk()
     BinaryClock.title("Binary Clock")
 
-    label = Label(BinaryClock, text=binary_time(hour, minute, second))
+    # Label
+    label = Label(BinaryClock, text="Current Time")
     label.pack()
 
+    # Canvas
+    canvas = Canvas(BinaryClock, width=420, height=420)
+    canvas.pack()
+    
+    # Clock Background, Image Copyright Algot Runeman 2013, CC0 (Creative Commons), http://runeman.org/articles/binary-clock/
+    clock = PhotoImage(file="BinaryClock.png")    
+    canvas.create_image(0, 0, anchor=NW, image=clock)
+
+    # Time Indicators
+    hour_1 = PhotoImage(file="dot.png")
+    hour_2 = PhotoImage(file="dot.png")
+    hour_01 = PhotoImage(file="dot.png")
+    hour_02 = PhotoImage(file="dot.png")
+    hour_04 = PhotoImage(file="dot.png")
+    hour_08 = PhotoImage(file="dot.png")
+
+    min_1 = PhotoImage(file="dot.png")
+    min_2 = PhotoImage(file="dot.png")
+    min_4 = PhotoImage(file="dot.png")
+    min_01 = PhotoImage(file="dot.png")
+    min_02 = PhotoImage(file="dot.png")
+    min_04 = PhotoImage(file="dot.png")
+    min_08 = PhotoImage(file="dot.png")
+
+    sec_1 = PhotoImage(file="dot.png")
+    sec_2 = PhotoImage(file="dot.png")
+    sec_4 = PhotoImage(file="dot.png")
+    sec_01 = PhotoImage(file="dot.png")
+    sec_02 = PhotoImage(file="dot.png")
+    sec_04 = PhotoImage(file="dot.png")
+    sec_08 = PhotoImage(file="dot.png")
+
+    canvas.create_image(7, 209, anchor=NW, image=hour_1)
+    canvas.create_image(7, 161, anchor=NW, image=hour_2)
+    canvas.create_image(55, 209, anchor=NW, image=hour_01)
+    canvas.create_image(55, 161, anchor=NW, image=hour_02)
+    canvas.create_image(55, 113, anchor=NW, image=hour_04)
+    canvas.create_image(55, 65, anchor=NW, image=hour_08)
+
+    canvas.create_image(103, 209, anchor=NW, image=min_1)
+    canvas.create_image(103, 161, anchor=NW, image=min_2)
+    canvas.create_image(103, 113, anchor=NW, image=min_4)
+    canvas.create_image(151, 209, anchor=NW, image=min_01)
+    canvas.create_image(151, 161, anchor=NW, image=min_02)
+    canvas.create_image(151, 113, anchor=NW, image=min_04)
+    canvas.create_image(151, 65, anchor=NW, image=min_08)
+    
+    canvas.create_image(199, 209, anchor=NW, image=sec_1)
+    canvas.create_image(199, 161, anchor=NW, image=sec_2)
+    canvas.create_image(199, 113, anchor=NW, image=sec_4)
+    canvas.create_image(247, 209, anchor=NW, image=sec_01)
+    canvas.create_image(247, 161, anchor=NW, image=sec_02)
+    canvas.create_image(247, 113, anchor=NW, image=sec_04)
+    canvas.create_image(247, 65, anchor=NW, image=sec_08)
+    
+   
     BinaryClock.mainloop()
 
     return 0;

77aa0bcea7d3eed8209c31c83fe19f23c3e9dc3b
Lane Moseley <moseley.lane@gmail.com>
2020-05-13 00:46:19 -0600

    Update BinaryClock.py
    Created constants for indicator positions.

--- BinaryClock/BinaryClock.py
+++ BinaryClock/BinaryClock.py
@@ -9,6 +9,17 @@ Description: The program retrieves the current time and displays it to the
 from tkinter import *
 from datetime import datetime
 
+HOURS_10 = 7
+HOURS_1 = 55
+MINUTES_10 = 103
+MINUTES_1 = 151
+SECONDS_10 = 199
+SECONDS_1 = 247
+COL_1 = 209
+COL_2 = 161
+COL_4 = 113
+COL_8 = 65
+
 # Tkinter
 BinaryClock = Tk()
 BinaryClock.title("Binary Clock")
@@ -24,7 +35,7 @@ canvas.create_image(0, 0, anchor=NW, image=clock)
 
 # Indicator Image
 indicator = PhotoImage(file="dot.png")
-  
+
 
 def display_clock():
     """
@@ -45,89 +56,89 @@ def display_clock():
     while hour > 0:
         # Hours
         if hour > 20:
-            canvas.create_image(7, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(HOURS_10, COL_2, anchor=NW, image=indicator, tags="ind")
             hour -= 20
         elif hour > 10:
-            canvas.create_image(7, 209, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(HOURS_10, COL_1, anchor=NW, image=indicator, tags="ind")
             hour -= 10
         # Hours < 10
         elif hour % 8 == 0:
-            canvas.create_image(55, 65, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(HOURS_1, COL_8, anchor=NW, image=indicator, tags="ind")
             hour -= 8
         elif hour % 4 == 0:
-            canvas.create_image(55, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(HOURS_1, COL_4, anchor=NW, image=indicator, tags="ind")
             hour -= 4
         elif hour % 2 == 0:
-            canvas.create_image(55, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(HOURS_1, COL_2, anchor=NW, image=indicator, tags="ind")
             hour -= 2
         else:
-            canvas.create_image(55, 209, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(HOURS_1, COL_1, anchor=NW, image=indicator, tags="ind")
             hour -= 1
     
     while minute > 0:
         # Minutes
         if minute > 50:
-            canvas.create_image(103, 209, anchor=NW, image=indicator, tags="ind")
-            canvas.create_image(103, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_1, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_4, anchor=NW, image=indicator, tags="ind")
             minute -= 50
         elif minute > 40:
-            canvas.create_image(103, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_4, anchor=NW, image=indicator, tags="ind")
             minute -= 40
         elif minute > 30:
-            canvas.create_image(103, 209, anchor=NW, image=indicator, tags="ind")
-            canvas.create_image(103, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_1, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_2, anchor=NW, image=indicator, tags="ind")
             minute -= 30
         elif minute > 20:
-            canvas.create_image(103, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_2, anchor=NW, image=indicator, tags="ind")
             minute -= 20
         elif minute > 10:
-            canvas.create_image(103, 209, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_10, COL_1, anchor=NW, image=indicator, tags="ind")
             minute -= 10
         # Minutes < 10
         elif minute % 8 == 0:
-            canvas.create_image(151, 65, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_1, COL_8, anchor=NW, image=indicator, tags="ind")
             minute -= 8
         elif minute % 4 == 0:
-            canvas.create_image(151, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_1, COL_4, anchor=NW, image=indicator, tags="ind")
             minute -= 4
         elif minute % 2 == 0:
-            canvas.create_image(151, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_1, COL_2, anchor=NW, image=indicator, tags="ind")
             minute -= 2
         else:
-            canvas.create_image(151, 209, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(MINUTES_1, COL_1, anchor=NW, image=indicator, tags="ind")
             minute -= 1
    
     while second > 0:
         # Seconds
         if second > 50:
-            canvas.create_image(199, 209, anchor=NW, image=indicator, tags="ind")
-            canvas.create_image(199, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_1, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_4, anchor=NW, image=indicator, tags="ind")
             second -= 50
         elif second > 40:
-            canvas.create_image(199, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_4, anchor=NW, image=indicator, tags="ind")
             second -= 40
         elif second > 30:
-            canvas.create_image(199, 209, anchor=NW, image=indicator, tags="ind")
-            canvas.create_image(199, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_1, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_2, anchor=NW, image=indicator, tags="ind")
             second -= 30
         elif second > 20:
-            canvas.create_image(199, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_2, anchor=NW, image=indicator, tags="ind")
             second -= 20
         elif second > 10:
-            canvas.create_image(199, 209, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_10, COL_1, anchor=NW, image=indicator, tags="ind")
             second -= 10
         # Seconds < 10
         elif second % 8 == 0:
-            canvas.create_image(247, 65, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_1, COL_8, anchor=NW, image=indicator, tags="ind")
             second -= 8
         elif second % 4 == 0:
-            canvas.create_image(247, 113, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_1, COL_4, anchor=NW, image=indicator, tags="ind")
             second -= 4
         elif second % 2 == 0:
-            canvas.create_image(247, 161, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_1, COL_2, anchor=NW, image=indicator, tags="ind")
             second -= 2
         else:
-            canvas.create_image(247, 209, anchor=NW, image=indicator, tags="ind")
+            canvas.create_image(SECONDS_1, COL_1, anchor=NW, image=indicator, tags="ind")
             second -= 1
 
     # Refresh clock with current time
